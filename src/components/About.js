797054f16a27fcf247a3c3075cfd3bc1f004da5e
Neil <zooming.in@hotmail.com>
2021-06-14 22:38:47 +1000

    updates

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                <h1>This app returns a list of fires from the 2019 - 2020 Australian fire season that were recorded by the MODIS (Moderate Resolution Imaging Spectroradiometer) satellite sensor.</h1>
+            </div>
+        )
+    }
+}
--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react'
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+import About from "./About"
+import URL from "./URL"
+import Contact from "./Contact"
 
 export default class NavBarComp extends Component {
     render() {
-       return ( 
-        <div>
-            <Navbar bg="dark" variant={"dark"} expand="lg">
-                <Navbar.Brand href="#home">2019-2020 Australian Bushfire Season</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                    <Nav.Link href="#about">About</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <Nav.Link href="#contact">Contact</Nav.Link>
+       return (
+        <Router> 
+            <div>
+                <Navbar bg="dark" variant={"dark"} expand="lg">
+                    <Navbar.Brand href="#home">2019-2020 Australian Bushfire Season</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="#about">About</Nav.Link>
+                            <Nav.Link href="#url">URL</Nav.Link>
+                            <Nav.Link href="#contact">Contact</Nav.Link>
+                        </Nav>
 
-                    </Nav>
+                    </Navbar.Collapse>
+                    </Navbar>
+            </div>
+            <div>
+                <Switch>
+                    <Route path="/about">
+                        <About />
+                    </Route>
+                    <Route path="/url">
+                        <URL />
+                    </Route>
+                    <Route path="/contact">
+                        <Contact />
+                    </Route>
+                </Switch>
+            </div>
 
-                </Navbar.Collapse>
-                </Navbar>
-        </div>
+
+
+        </Router>
        )
     }
 }
